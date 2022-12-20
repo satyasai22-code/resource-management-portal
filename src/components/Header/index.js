6df250e270dc7a09fbe79d5882abd3d905d8d0dc
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2022-12-20 10:56:23 +0530

    Add Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import websiteLogo from '../../images/website_logo.png'
+import profile from '../../images/profile.png'
+import React from "react";
+import "./index.css";
+
+const Header = (props) => {
+  return (
+    <div className="nav-header">
+      <div className="nav-content">
+        <div className="nav-container">
+          <Link to="/">
+            <img
+              className="website-logo"
+              src={websiteLogo}
+              alt="website logo"
+            />
+          </Link>
+          <div className="button-logo-container">
+             <Link to="/resource/add">
+                <button type="button" className="add-item-btn">
+                  ADD ITEM
+                </button>
+              </Link>
+            <Link to="/login">
+                <img
+                    className="profile-logo"
+                    src={profile}
+                    alt="profile logo"
+                />
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from '../Header'
 
 const Home = () => {
     return (
-      <div>home</div>
+      <Header/>
     )
 }
 
