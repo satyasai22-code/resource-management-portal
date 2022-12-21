86babc35efeeed3ec5e1260616c600385473b743
Satya Sai Neelapala <neelapalasatyasai@gmail.com>
2022-12-21 13:10:46 +0530

    Add page Not Found

---
+++ src/components/PageNotFound/index.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import notFound from '../../images/not_found.png'
+import "./index.css";
+
+const PageNotFound = () => (
+  <div className="not-found-container">
+    <img
+      className="not-found-image"
+      src={notFound}
+      alt="not found"
+    />
+    <h1 className="not-found-heading">Page Not Found</h1>
+    <p className="not-found-description">
+      We are sorry, the page you requested could not be found. Please go back to
+      the homepage.
+    </p>
+    <Link to="/" className="go-back-home-link">
+      <button type="button" className="go-back-to-home-btn">
+        Go Back to Home
+      </button>
+    </Link>
+  </div>
+);
+
+export default PageNotFound;
